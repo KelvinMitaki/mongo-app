acac4425a8567a69e8a598d756764a686dbc3377
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-31 20:01:49 +0300

    fixed a small login bug

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,12 +18,11 @@ router.post("/login", async (req, res, next) => {
     const user = await getDb().db().collection("users").findOne({ email });
     // If yes, create token and return it to client
     const result = await bcrypt.compare(pw, user.password);
-    if (result) {
-      const token = createToken();
-      res
-        .status(200)
-        .json({ token: token, message: "Authentication succeeded" });
+    if (!result) {
+      throw new Error();
     }
+    const token = createToken();
+    res.status(200).json({ token: token, message: "Authentication succeeded" });
   } catch (error) {
     res.status(401).json({
       message: "Authentication failed, invalid username or password."
