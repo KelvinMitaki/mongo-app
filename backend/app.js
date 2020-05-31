7b544c43a7184f6f3c08bf30aa6286553098f1d3
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-31 15:37:18 +0300

    connected to mongo atlas

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,41 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
-const productRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth');
+const productRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join('backend/images')));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   // Set CORS headers so that the React SPA is able to communicate with this server
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.use('/products', productRoutes);
-app.use('/', authRoutes);
+app.use("/products", productRoutes);
+app.use("/", authRoutes);
+
+const mongoConnect = async () => {
+  try {
+    const client = await MongoClient.connect(process.env.MONGO_CLIENT, {
+      useUnifiedTopology: true
+    });
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+  }
+};
+mongoConnect();
 
 app.listen(3100);
